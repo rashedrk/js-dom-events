a519a4cda42f81809f703d57cbfaa6c18f1c5e7b
rashedrk <rashedrk001@gmail.com>
2023-02-13 12:45:38 +0600

    addeventlistener

--- js/events.js
+++ js/events.js
@@ -14,3 +14,20 @@ const makePurpleButton = document.getElementById('make-purple');
 makePurpleButton.onclick = function () {
     document.body.style.backgroundColor = 'purple';
 }
+
+// option 4
+const makePinkButton = document.getElementById('make-pink');
+makePinkButton.addEventListener('click', makePink);
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+// option 4 another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+});
+
+// option 4 final
+document.getElementById('make-goldenrod').addEventListener('click',function () {
+    document.body.style.backgroundColor = 'goldenrod';
+})
